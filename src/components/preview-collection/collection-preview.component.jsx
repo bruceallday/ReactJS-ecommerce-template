c1220ff067ff4d2cafde055a1e8d87a5f769f00f
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2020-01-06 14:30:35 +0000

    style: Shop images

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
@@ -7,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
         <div className='preview'>
             {items
                 .filter((item, idx) => idx < 4 )
-                .map((item) => (
-                <div key={item.id}>{item.name}</div>
+                .map(( {id, ...otherItemProps} ) => (
+                <CollectionItem key={id} {...otherItemProps} />
             ))}
         </div>
     </div>
