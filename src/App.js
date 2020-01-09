6d920c6c1d12c80fe7e41314cc32de0e147e93a2
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2020-01-09 17:51:15 -0500

    feat: Sign-in functionality

--- src/App.js
+++ src/App.js
@@ -30,8 +30,6 @@ class App extends React.Component{
               ...snapShot.data()
             }
           })
-
-          console.log(this.state)
         });
         
       }
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils.js';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils.js';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -15,10 +15,17 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({email: '', password: ''})
+        const {email, password} = this.state;
+
+        try{
+          await auth.signInWithEmailAndPassword(email, password);
+          this.setState({ email: "", password: "" });
+        }catch(error){
+          console.log(error)
+        }
     }
 
     handleChange = event => {
