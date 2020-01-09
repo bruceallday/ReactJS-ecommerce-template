ed6acf5a91b34d586d50d09cdd822b939f1570e3
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2020-01-09 13:44:13 -0500

    feat: Created CustomButton component

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './custom-button.styles.scss';
+
+const CustomButton = ({children, ...otherProps}) =>(
+    <button className='custom-button' {...otherProps} >
+        {children}
+    </button>
+)
+
+export default CustomButton;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -51,7 +51,8 @@ class SignIn extends React.Component{
                        required
                 />
 
-                <input type='submit' value='Submit Form'/>
+                <CustomButton type='submit'> Sign In </CustomButton>
+
               </form>
             </div>
         )
