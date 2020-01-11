9645aca93051b8cebe40eefbd468e2fabb8e7f58
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-10 23:30:16 -0500

    style: custom button invert on hover

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component.jsx';
+
 import './collection-item.styles.scss';
+
 
 const CollectionItem = ({id, name, price, imageUrl}) =>(
     <div className='collection-item'>
@@ -14,6 +17,8 @@ const CollectionItem = ({id, name, price, imageUrl}) =>(
             <span className='name'> {name} </span>
             <span className='price'> {price} </span>
         </div>
+
+        <CustomButton inverted >Add to cart</CustomButton>
     </div>
 )
 
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) =>(
-    <button className={`${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} 
-        {...otherProps}
-    >
-        {children}
-    </button>
-)
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={` ${inverted ? "inverted" : ""} ${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
 
 export default CustomButton;
