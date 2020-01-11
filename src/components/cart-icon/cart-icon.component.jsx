a9b1d93dec7cdf80101cf95e8a8983c17534a8ea
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-11 13:44:54 -0500

    feat: added cart-icon mapStateToProps method

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,13 +7,20 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'> 0 </span>
+        <span className='item-count'> { itemCount } </span>
     </div>
 
 )
+
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    itemCount: cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+        0
+    )
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
@@ -22,7 +29,7 @@ const mapDispatchToProps = dispatch => ({
 
 
 export default connect (
-    null, 
+    mapStateToProps, 
     mapDispatchToProps)
     (CartIcon);
     
