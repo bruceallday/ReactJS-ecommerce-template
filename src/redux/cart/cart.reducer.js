433dcac393f587cb77d8f0889eb479e83c66e9d1
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-11 12:36:40 -0500

    feat: created card.utils.js file and add quantity to cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import CartActionTypes from './card.types.js';
+import {addItemsToCart} from './cart.utils.js';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -17,7 +18,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.ADD_ITEM:
             return{
                 ...state,
-                cartItems:[...state.cartItems, action.payload]
+                cartItems: addItemsToCart(state.cartItems, action.payload )
             }
 
             default:
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemsToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id 
+    )
+
+    if(existingCartItem){
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        )
+    }
+
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
