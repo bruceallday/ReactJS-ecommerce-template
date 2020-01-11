7658e62904c3059d8f40353e931bf3094505c46a
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-11 18:37:27 -0500

    feat: checkoutItem component

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './checkout-item.styles.scss';
+
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+  <div className="checkout-item">
+    <div className="image-container">
+      <img src={imageUrl} alt="item" />
+    </div>
+    <span className="name">{name}</span>
+    <span className="quantity">{quantity}</span>
+    <span className="price">{price}</span>
+    <div className='remove-button'>&#10005;</div>
+  </div>
+);
+
+export default CheckoutItem
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 
 import './checkout.styles.scss';
 
@@ -30,7 +31,7 @@ const CheckoutPage = ({cartItems, total}) => (
     </div>
     {
     cartItems.map(cartItem =>
-        cartItem.name
+        (<CheckoutItem key={cartItem.id} cartItem={cartItem}/>)
         )
     }
     <div className='total'>
