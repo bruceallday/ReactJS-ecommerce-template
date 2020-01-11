af5202ad8a8d3b2a1b7606b92f554fd5dcc07ca7
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-11 14:37:03 -0500

    feat: cart and user selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+
 import {auth} from '../../firebase/firebase.utils.js';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component.jsx';
+import { selectCartHidden } from '../../redux/cart/cart.selectors.js';
+import { selectCurrentUser } from '../../redux/user/user.selector.js';
 
 import './header.styles.scss';
 
@@ -40,9 +45,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({user: { currentUser }, cart:{ hidden }}) =>({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header)
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,6 +7,11 @@ export const selectCartItems = createSelector(
     cart => cart.cartItems
 )
 
+export const selectCartHidden = createSelector(
+     [selectCart],
+     cart => cart.hidden
+)
+
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems =>
