6cfa7e11e54cf379b54c003bbb10206065977fe5
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-11 18:04:49 -0500

    feat: trigger toggleCartHidden on link to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,12 +4,15 @@ import { createStructuredSelector } from "reselect";
 import { withRouter } from 'react-router-dom';
 
 import CartItem from '../cart-item/cart-item.component';
+import CustomButton from "../custom-button/custom-button.component.jsx";
+
 import { selectCartItems } from "../../redux/cart/cart.selectors";
-import CustomButton from '../custom-button/custom-button.component.jsx';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) =>(
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -21,7 +24,13 @@ const CartDropdown = ({ cartItems, history }) =>(
                 <span className='empty-message'>Your cart is empty</span>
                 )}
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}> GO TO CHECKOUT </CustomButton>
+        <CustomButton 
+            onClick={() => {
+                history.push('/checkout');
+                dispatch(toggleCartHidden())
+            }}> 
+            GO TO CHECKOUT 
+        </CustomButton>
     </div>
 )
 
