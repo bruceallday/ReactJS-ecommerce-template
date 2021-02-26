251b6b4a049c02472b3d497a08531f61b7fac6a1
unknown <nkhs@yandex.com>
2021-02-27 04:56:38 +0800

    Upgrade react version, Added minium test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { shallow, mount } from 'enzyme/build'
 import { render } from '@testing-library/react';
 import App from './App';
+import { store, persistor } from './redux/store.js'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+import CartDropdown from './components/cart-dropdown/cart-dropdown.component';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+it('mounts App without crashing', () => {
+  mount(
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  )
+})
+
+it('mounts CartDropdown with empty data', () => {
+  let wrapper = mount(<Provider store={store}><BrowserRouter><CartDropdown /></BrowserRouter></Provider>)
+  expect(wrapper.find('.empty-message').last().text()).toEqual('Your cart is empty');
+})
